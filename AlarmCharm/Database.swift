db72af55722bdff489dfacfaa487be37fb689fd3
Alexander Carlisle <waba@stanford.edu>
2016-05-28 12:41:01 -0700

    Added ability to save file from database to local, read file from local and play it, and set it as local notification

--- AlarmCharm/Database.swift
+++ AlarmCharm/Database.swift
@@ -11,7 +11,6 @@ import Firebase
 
 class Database {
     
-    
     private var usersRef = FIRDatabase.database().reference().child("users")
     
     func uploadFileToDatabase(fileURL: NSURL, forUser userID: String) {
@@ -31,11 +30,85 @@ class Database {
                 currUserRef.updateChildValues(newSound)
             }
         }
-
+        
     }
+    
+    
+    
     
     func downloadFileToLocal(forUser userID: String) {
         //Carlisle your function will go here!
+        usersRef.child(userID).observeSingleEventOfType(.Value, withBlock: { (snapshot) in
+            if let audioFile = snapshot.value!["audio_file"] as? String where audioFile != ""{
+                let storage = FIRStorage.storage()
+                let gsReference = storage.referenceForURL("gs://project-5208532535641760898.appspot.com")
+                let soundRef = gsReference.child(audioFile)
+                soundRef.downloadURLWithCompletion { (URL, error) -> Void in
+                    if (error != nil) {
+                        print(error)
+                    } else {
+                        self.saveToFileSystem(URL!, fileName: "test.caf")
+                    }
+                }
+            }
+        })
+        { (error) in
+            print(error)
+        }
     }
+    
+    /*
+     Given the url, it turns url into NSDATA and then saves the file in the libray/sounds folder.
+     */
+    private func saveToFileSystem(URL : NSURL, fileName: String){
+        let songData =  NSData(contentsOfURL: URL)
+        let fileManager = NSFileManager.defaultManager()
+        
+        let libraryPath = NSSearchPathForDirectoriesInDomains(.LibraryDirectory, .UserDomainMask, true)[0]
+        let soundsPath = libraryPath + "/Sounds"
+        let filePath = soundsPath + "/" + fileName
+        do {
+            try fileManager.createDirectoryAtPath(soundsPath, withIntermediateDirectories: false, attributes: nil)
+        } catch let error1 as NSError {
+            print("error" + error1.description)
+        }
+        let soundPathUrl = NSURL(fileURLWithPath: filePath)
+        print(soundPathUrl)
+        songData?.writeToURL(soundPathUrl,  atomically: true)
+    }
+    
+    //GOOD FOR TESTING READING FROM FILE SYSTEM ONCE WE HAVE WRITTEN TO IT
+    private func getURlFromFileSystem(fileName: String) -> NSURL{
+        let libraryPath = NSSearchPathForDirectoriesInDomains(.LibraryDirectory, .UserDomainMask, true)[0]
+        let soundsPath = libraryPath + "/Sounds"
+        let filePath = soundsPath + "/" + fileName
+        let fileManager = NSFileManager.defaultManager()
+        
+        do {
+            try fileManager.createDirectoryAtPath(soundsPath, withIntermediateDirectories: false, attributes: nil)
+        } catch let error1 as NSError {
+            print("error" + error1.description)
+        }
+        let myURL = NSURL(fileURLWithPath: filePath)
+        return myURL
+    }
+    
+    /*
+     This function will be moved to Notification Class soon
+     Grabs the notificationm and changes the soundName to be the sound name of the user
+     */
+    func setNotificationFromFileSystem(){
+        let notifications = UIApplication.sharedApplication().scheduledLocalNotifications
+        if notifications?.count > 0 {
+            if let oldNotification = notifications?[0]
+            {
+                oldNotification.soundName = "test.caf"
+                UIApplication.sharedApplication().cancelAllLocalNotifications()
+                UIApplication.sharedApplication().scheduleLocalNotification(oldNotification)
+            }
+        }
+    }
+    
+    
     
 }
